b7ccfd45169fc4562369e05b498cdee059f49a12
chriss1525 <okothchris15@gmail.com>
2023-05-09 16:44:47 +0300

    create to_dict and save methods

--- models/base_model.py
+++ models/base_model.py
@@ -15,15 +15,25 @@ class BaseModel:
     """
 
     def __init__(self):
+        # declare attributes
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-    def update(self):
+    def save(self):
+        # update updated_at everytime the object is changed
         self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
-model = BaseModel()
-print(model)
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()  # Create a copy of instance attributes
+        obj_dict['__class__'] = self.__class__.__name__  # Add class name
+
+        # Convert created_at and updated_at to ISO format strings
+        for attr, value in obj_dict.items():
+            if isinstance(value, datetime):
+                obj_dict[attr] = value.isoformat()
+
+        return obj_dict
