d38bab74ecc7ea87b1a828c743f1f86c4949614f
chriss1525 <okothchris15@gmail.com>
2023-05-10 21:42:48 +0300

    trying to add create command

--- console.py
+++ console.py
@@ -8,19 +8,37 @@ do_quit - handles quit command
 do_EOF - handles exit using EOF
 """
 import cmd
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """Exit the program"""
+        """type quit to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """Exit the program"""
+        """Ctrl + D exits the program"""
         print()  # Print a new line before exiting
         return True
+
+    def do_create(self, arg):
+        """create command creates a new instance of a specified class"""
+        if not arg:
+            print('** class name missing **')
+            return
+
+        class_name = arg.strip()
+        if class_name not in ["BaseModel"]:
+            print('** class doesn\'t exist **')
+            return
+
+        new_instance = BaseModel()
+
+        new_instance.save()
+        print(new_instance.id)
 
 
 if __name__ == '__main__':
